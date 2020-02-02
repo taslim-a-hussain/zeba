abb9389111342c66e33b38ba960c0f213c280e5d
taslim-a-hussain <taslim@outlook.co.nz>
2020-02-02 23:45:00 +1300

    Second commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Zeba
+A lightweight JavaScript library for validating, manipulating, and formatting strings.
+
+## Install:
+> npm i zeba
+
+### Require:
+> `const` str = `require`(`'zeba'`);
+
+### How to use:
+#### _Example:_
+> `let` input = '&nbsp;&nbsp;&nbsp;&nbsp;this&nbsp;&nbsp;&nbsp; is the&nbsp;&nbsp;&nbsp;&nbsp; input.';
+>
+> input = `new` str(input).fullTrim().capitilize();
+>
+> `console.log`(input.string);
+#### _Outcome:_
+This is the input.
+
+____
+
---
+++ Zeba.js
@@ -0,0 +1,42 @@
+class Zeba {
+
+    constructor(str) {
+        this.string = str;
+    }
+
+    capitilize () {
+        this.string = this.string.charAt(0).toUpperCase() + this.string.slice(1);
+        return this;
+    }
+
+    upper() {
+        try {
+            this.string = this.string.split(" ");
+            for (let i = 0, x = this.string.length; i < x; i++) {
+                this.string[i] = this.string[i][0].toUpperCase() + this.string[i].substr(1);
+        
+            }
+            this.string = this.string.join(" ");
+            return this;       
+        } catch {
+            return this;
+        }
+    }
+
+    allupper() {
+        this.string = this.string.toUpperCase();
+        return this;
+    }
+
+    fullTrim (change = false) {
+        this.string = this.string.trim();
+        change = change ? change : ' ';
+        this.string = this.string.replace(/\s+/g, change);
+        return this;
+    }
+
+
+} // End of the Class
+
+
+module.exports = Zeba;
